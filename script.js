c75c3cfc84a490040797b983a1a9aef92e843479
TripleT511 <0306191074@caothang.edu.vn>
2021-10-20 16:05:42 +0700

    Update css and js

--- script.js
+++ script.js
@@ -1,23 +1,38 @@
 /* carousel */
 const backIcon = '<img src="./images/arrow-back.svg" alt="left">';
 const nextIcon = '<img src="./images/arrow-next.svg" alt="right">';
-$('#slider').owlCarousel({
-    loop: true,
-    margin: 50,
-    nav: true,
-    navText: [
-        backIcon,
-        nextIcon
-    ],
-    responsive: {
-        0: {
-            items: 1,
+$('.slider').slick({
+    dots: true,
+    infinite: false,
+    speed: 300,
+    slidesToShow: 3,
+    slidesToScroll: 3,
+    prevArrow: `<button type='button' class='slick-prev pull-left'><img src='./images/arrow-back.svg'></button>`,
+    nextArrow: `<button type='button' class='slick-next pull-right'><img src='./images/arrow-next.svg'></button>`,
+    responsive: [
+        {
+            breakpoint: 1024,
+            settings: {
+                slidesToShow: 3,
+                slidesToScroll: 3,
+                infinite: true,
+                dots: true
+            }
         },
-        768: {
-            items: 2,
+        {
+            breakpoint: 600,
+            settings: {
+                slidesToShow: 2,
+                slidesToScroll: 2
+            }
         },
-        1000: {
-            items: 2.7,
+        {
+            breakpoint: 480,
+            settings: {
+                slidesToShow: 1,
+                slidesToScroll: 1
+            }
         }
-    }
-})
+
+    ]
+});
