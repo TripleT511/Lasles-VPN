8cc5df3c408f015c8bb2d8e22af629e553332be5
TripleT511 <0306191074@caothang.edu.vn>
2021-10-21 20:53:02 +0700

    Update reponsive part 2

--- script.js
+++ script.js
@@ -20,14 +20,14 @@ $('.slider').slick({
             }
         },
         {
-            breakpoint: 600,
+            breakpoint: 700,
             settings: {
                 slidesToShow: 2,
                 slidesToScroll: 2
             }
         },
         {
-            breakpoint: 480,
+            breakpoint: 580,
             settings: {
                 slidesToShow: 1,
                 slidesToScroll: 1
